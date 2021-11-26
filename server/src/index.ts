f1aea5e844b8de4996e380f1ed51ef645a4641ce
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-11-27 12:05:35 +1300

    server basic api endpoints (non-ws)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,15 @@
-import { globalLogger } from './models/logger';
 import ServerHub from './models/serverHub';
 
 export const serverHub = new ServerHub(3001);
 
 serverHub.createGame({ ip: '127.0.0.1', username: 'Server', token: '' }, 'dev');
+
+serverHub.app.get('/', (_, res) => {
+    res.status(200).send(
+        "Congratulations, you found the mafia server endpoint! Shame you're not a websocket.",
+    );
+});
+
+serverHub.app.get('/status', (_, res) => {
+    res.status(200).send(`Server is up with ${serverHub.getNumberOfGames()} game(s)`);
+});

a87f720a4e10ce5a701a6b882d1f4123f2376f33
NachoToast <32235595+NachoToast@users.noreply.github.com>
2021-11-30 16:42:40 +1300

    Endpoint changing tweaks

--- client/src/constants/endpoints.ts
+++ client/src/constants/endpoints.ts
@@ -1,8 +1,20 @@
+interface PossibleOverrides {
+    serverEndpoint?: string;
+    serverPort?: number;
+    serverName?: string;
+}
+
+let overrides: PossibleOverrides | undefined;
+
+try {
+    overrides = require('../config/endPointOverrides.json') as PossibleOverrides;
+} catch {}
+
 /** Endpoint for the websocket to hit. */
-export const serverEndpoint: string = 'http://ntgc.ddns.net';
+export const serverEndpoint: string = overrides?.serverEndpoint || 'http://ntgc.ddns.net';
 
 /** Port for the websocket to use. */
-export const serverPort: number = 3001;
+export const serverPort: number = overrides?.serverPort || 3001;
 
 /** Name of the server hub. */
-export const serverName: string = 'mafia';
+export const serverName: string = overrides?.serverName || 'mafia';

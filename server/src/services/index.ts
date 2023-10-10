42a8eab6d21deee1112f74a7e1f75c58c50cde4c
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-10-11 02:03:18 +1300

    add new server initialisation

---
+++ server/src/services/index.ts
@@ -0,0 +1,5 @@
+import * as AuthenticationService from './AuthenticationService';
+import * as DiscordService from './DiscordService';
+import * as UserService from './UserService';
+
+export { AuthenticationService, DiscordService, UserService };

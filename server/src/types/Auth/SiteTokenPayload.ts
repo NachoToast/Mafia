959dbf6472846c4bf561a6dc1140201cb7a9fbf5
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-10-11 02:01:07 +1300

    add auth types

---
+++ server/src/types/Auth/SiteTokenPayload.ts
@@ -0,0 +1,38 @@
+import { DiscordIDString } from '../Utility';
+
+/**
+ * Shape of the payload that is stored in the body of a site token
+ * (a signed JWT).
+ *
+ * Site tokens should be provided in the `authorization` header
+ * when making elevated requests.
+ */
+export interface SiteTokenPayload {
+    /**
+     * The Discord user ID of the requester.
+     *
+     * This can be used to see if they exist in our users database.
+     */
+    id: DiscordIDString;
+
+    /**
+     * The Discord OAuth2 access token of the requester.
+     *
+     * This is used to make elevated requests to the Discord API
+     * on behalf of this user.
+     *
+     * Since we only require the 'Identify' scope,
+     * the only requests we can make are to:
+     * - Terminate the current OAuth session (revoke).
+     * - Extend the current OAuth session (refresh).
+     * - Get basic user info (ID, username, profile picture, etc...)
+     */
+    access_token: string;
+
+    /**
+     * The Discord OAuth2 refresh token of the requester.
+     *
+     * This can be used to extend the user's current OAuth session.
+     */
+    refresh_token: string;
+}

a1790675f0d05ae2a79e6715c0c5a86dd4bcc93d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-10-14 17:54:43 +1300

    add `config.hash` support for non-git environments

--- server/src/constants/defaultConfig.ts
+++ server/src/constants/defaultConfig.ts
@@ -2,6 +2,15 @@ import { execSync } from 'child_process';
 import { randomBytes } from 'crypto';
 import { Config } from '../types/Config';
 
+function getCommit(): Config['commit'] {
+    try {
+        return execSync('git rev-parse HEAD').toString().trim();
+    } catch {
+        // If the git command fails, we're probably not in a git repo.
+        return 'unknown';
+    }
+}
+
 /**
  * Default config values, keep this in sync with `.github/config-schema.json`.
  */
@@ -23,9 +32,9 @@ export const defaultConfig: Omit<Config, 'mongoURI'> = {
     mongoDbName: 'mafia_default',
 
     /**
-     * This ensures cryptographic security,
-     * but also means it will change every time the server restarts,
-     * meaning user sessions will not persist between server restarts either.
+     * This ensures cryptographic security, but also means it
+     * will change every time the server restarts, so user sessions
+     * will not persist between server restarts either.
      *
      * This default is undocumented in the schema
      * as it cannot be faithfully represented.
@@ -44,7 +53,7 @@ export const defaultConfig: Omit<Config, 'mongoURI'> = {
      */
     discordClientSecret: 'dummy Discord client secret',
 
-    commit: execSync('git rev-parse HEAD').toString().trim(),
+    commit: getCommit(),
 
     startedAt: new Date().toISOString(),
 };

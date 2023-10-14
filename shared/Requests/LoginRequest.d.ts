bddbbf7dbc42b1faeb572f546e3b4e6989642825
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-10-15 00:37:51 +1300

    add comments to `LoginRequest.code`

--- shared/Requests/LoginRequest.d.ts
+++ shared/Requests/LoginRequest.d.ts
@@ -1,5 +1,6 @@
 /** Request body for the login endpoint. */
 export interface LoginRequest {
+    /** OAuth2 authorisation code given from Discord. */
     code: string;
 
     redirect_uri: string;
